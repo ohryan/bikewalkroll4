90ce36c5b9963cea5b9e80f8d01c5bdc7ff2476c
Ryan Neudorf <ryan.neudorf@gmail.com>
2020-09-07 23:04:33 -0500

    Adjusted navbar breakpoint

--- src/components/header/Navbar.vue
+++ src/components/header/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
-  <header class="py-3 px-6 sm:flex sm:justify-between sm:items-center">
+  <header class="py-3 px-6 lg:flex lg:justify-between lg:items-center">
     <div class="flex items-center justify-between">
-      <div class="sm:hidden">
+      <div class="lg:hidden">
         <button @click="isOpen = !isOpen" type="button" class="block text-default hover:text-green focus:text-green focus:outline-none">
           <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
             <path v-if="isOpen" fill-rule="evenodd" d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"/>
@@ -12,20 +12,20 @@
       <div>
         <router-link to="/" @click.native="isOpen = false"><img src="https://bikewalkroll.org/img/logo-v2.png" alt="BikeWalkRoll Logo" /></router-link>
       </div>
-      <div class="sm:hidden">
+      <div class="lg:hidden">
         <router-link to="/" class="py-3 font-thin text-green">Sign In</router-link>
       </div>
     </div>
-    <nav :class="isOpen ? 'block' : 'hidden'" class="sm:flex mt-3 sm:mt-0">
-        <ul class="sm:space-x-4">
-          <li v-for="n in nav" :key="n.routeName" class="hover:text-green sm:inline-block">
+    <nav :class="isOpen ? 'block' : 'hidden'" class="lg:flex mt-3 lg:mt-0">
+        <ul class="lg:space-x-3">
+          <li v-for="n in nav" :key="n.routeName" class="hover:text-green lg:inline-block">
             <router-link :to="{name: n.routeName}" @click.native="isOpen = false" class="py-2">{{ n.title }}</router-link>
           </li>
         </ul>
     </nav>
-    <div class="hidden sm:flex justify-between space-x-1 mt-0">
-      <button class="btn-sm btn-green flex-1 sm:flex-auto">Create an Accont</button>
-      <button class="btn-sm btn-green-invert flex-1 sm:flex-auto">Sign In</button>
+    <div class="hidden lg:flex justify-between space-x-1 mt-0">
+      <button class="btn-sm btn-green flex-1 lg:flex-auto">Create an Accont</button>
+      <button class="btn-sm btn-green-invert flex-1 lg:flex-auto">Sign In</button>
     </div>
   </header>
 </template>

4a346820edfce2d33c790d86334882ef800382b9
Ryan Neudorf <ryan.neudorf@gmail.com>
2020-09-14 22:53:11 -0500

    static page layout tweaks

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,32 +2,35 @@
   <div class="about static-page">
     <SubHeader title="About Us"></SubHeader>
     <ContentMain
-      title="About the design team and vision behind BikeWalkRoll"
+      :dark=true
     >
-      <p><strong>Our vision</strong> is a world where young people of any age get to experience the joy and freedom of biking, walking or using a skateboard/wheelchair to get to school on one’s own.</p>
-      <p><strong>Our mission</strong> is to enlist the effort of school classrooms around the world to help reinvent how cities, towns and countries measure transportation. Period. (We see school transportation in the same way that others might see commuting to work - a useful benchmark for measuring the health of our entire transportation - except that adults tend to have their minds made up and are very hard to survey….. Kids are different. We think that we can collect just as important data, faster, easier and share it when/where/how it counts. We think BikeWalkRoll is more than childsplay, BikeWalkRoll is a new way of counting transportation.)</p>
+      <div class="max-w-screen-lg m-auto p-6 bg-white">
+        <h1>About the design team and vision behind BikeWalkRoll</h1>
+        <p><strong>Our vision</strong> is a world where young people of any age get to experience the joy and freedom of biking, walking or using a skateboard/wheelchair to get to school on one’s own.</p>
+        <p><strong>Our mission</strong> is to enlist the effort of school classrooms around the world to help reinvent how cities, towns and countries measure transportation. Period. (We see school transportation in the same way that others might see commuting to work - a useful benchmark for measuring the health of our entire transportation - except that adults tend to have their minds made up and are very hard to survey….. Kids are different. We think that we can collect just as important data, faster, easier and share it when/where/how it counts. We think BikeWalkRoll is more than childsplay, BikeWalkRoll is a new way of counting transportation.)</p>
 
-    <h3>We Value:</h3>
-      <ul class="list-disc list-inside">
-        <li>Open data</li>
-        <li>Real-time results</li>
-        <li>Ease of use</li>
-        <li>Story telling</li>
-        <li>Citizen Science</li>
-        <li>Friendly Competition (...with emphasis on repeating surveys and self-improvement, not just on overall BikeWalkRoll scores. Sure, we all want these to go up, but a low score is not a school/kid/parent’s fault. After all, 99% of students would rather get to school the healthy and fun way. It is not enough to encourage people. We face real barriers. Building better cities something we all need to do something about.)</li>
-      </ul>
-      <h3>The Team:</h3>
-      <p><a href="https://twitter.com/swansonanders" target="_blank">Anders Swanson</a> is the project director. He charts the course, makes phone calls, answers emails, tests for bugs, draws stuff occasionally, tweets too much, talks to big crowds of people, cleans the toilets and does a little bit of everything. </p>
-      <p><a href="https://twitter.com/ohryan" target="_blank">Ryan Neudorf</a> is the web developer. He is the head wizard responsible for making it work well on your computer.</p>
-      <p><a href="https://twitter.com/ty_candy_" target="_blank">Tyler Johnston</a> is an graphic designer. He is responsible for making everything look nice and juicy and fun.</p>
-      <p><a href="https://twitter.com/greenactionctr">Green Action Centre</a> is the not-for-profit organization for whom the software is developed. They find the money and write the cheques. More importantly…. their Active and Safe Routes to School program is on the front lines of school transportation every day and all of their hard work inspired and formed the basis for this software. And, of course, they take the lead on making BikeWalkRoll grow at home in Manitoba.</p>
-      <p><strong>You</strong> are the ones that really make BikeWalkRoll work. Help it spread. You can do so by telling teachers, politicians, parents, about the program and why it is so very important (and easy!) to do a BikeWalkRoll survey. If you work for an organization, and want to find out more about getting funded to run a campaign near you, don’t hesitate to send us an email. </p>
-      <h3>Translation support kindly provided by:</h3>
-      <a href="http://arquitecturia.org/" target="_blank"><img src="/img/LOGO AS ARQ NB.png" alt="Arquitectúria" class="m-auto p-6"></a>
-      <a href="https://pyoraliitto.fi/" target="_blank"><img src="/img/PyorallitoLogo.png" alt="pyorallito" class="m-auto p-6"></a>
-      
-      <h4>Acknowledgements:</h4>
-      <p>Over the years, the following people have helped make BikeWalkRoll possible: Devon Mitton (web wizard #2), Torrin Swanson (campaign and mobile developer), Christine Houde, Sue Barkman, Randall McQuaker, Jackie Avent, Shoni Madden, Lea Grzenda, Janice Lukes, Tracy Hucul, Masha Giller, Jamie Hilland, Red River College, The University of Manitoba, The City of Winnipeg, Adam Prokopanko, Steve Lawrence, Toronto Catholic School Division, The National Safe Routes To School Partnership, Taylor Lonsdale, The Winnipeg Foundation, The Heart and Stroke Foundation, Jeannette Montufar, Jamie Lacoste, Leigh Anne Parry, Velo-City Vienna, Walk21, Winter Cycling Federation, Torrin Swanson, International Winter Bike to Work Day, Neil Bailey, Green Communities Canada, school division, students and teachers everywhere!</p>
+        <h3>We Value:</h3>
+          <ul class="list-disc list-inside">
+            <li>Open data</li>
+            <li>Real-time results</li>
+            <li>Ease of use</li>
+            <li>Story telling</li>
+            <li>Citizen Science</li>
+            <li>Friendly Competition (...with emphasis on repeating surveys and self-improvement, not just on overall BikeWalkRoll scores. Sure, we all want these to go up, but a low score is not a school/kid/parent’s fault. After all, 99% of students would rather get to school the healthy and fun way. It is not enough to encourage people. We face real barriers. Building better cities something we all need to do something about.)</li>
+          </ul>
+          <h3>The Team:</h3>
+          <p><a href="https://twitter.com/swansonanders" target="_blank">Anders Swanson</a> is the project director. He charts the course, makes phone calls, answers emails, tests for bugs, draws stuff occasionally, tweets too much, talks to big crowds of people, cleans the toilets and does a little bit of everything. </p>
+          <p><a href="https://twitter.com/ohryan" target="_blank">Ryan Neudorf</a> is the web developer. He is the head wizard responsible for making it work well on your computer.</p>
+          <p><a href="https://twitter.com/ty_candy_" target="_blank">Tyler Johnston</a> is an graphic designer. He is responsible for making everything look nice and juicy and fun.</p>
+          <p><a href="https://twitter.com/greenactionctr">Green Action Centre</a> is the not-for-profit organization for whom the software is developed. They find the money and write the cheques. More importantly…. their Active and Safe Routes to School program is on the front lines of school transportation every day and all of their hard work inspired and formed the basis for this software. And, of course, they take the lead on making BikeWalkRoll grow at home in Manitoba.</p>
+          <p><strong>You</strong> are the ones that really make BikeWalkRoll work. Help it spread. You can do so by telling teachers, politicians, parents, about the program and why it is so very important (and easy!) to do a BikeWalkRoll survey. If you work for an organization, and want to find out more about getting funded to run a campaign near you, don’t hesitate to send us an email. </p>
+          <h3>Translation support kindly provided by:</h3>
+          <a href="http://arquitecturia.org/" target="_blank"><img src="/img/LOGO AS ARQ NB.png" alt="Arquitectúria" class="m-auto p-6"></a>
+          <a href="https://pyoraliitto.fi/" target="_blank"><img src="/img/PyorallitoLogo.png" alt="pyorallito" class="m-auto p-6"></a>
+          
+          <h4>Acknowledgements:</h4>
+          <p>Over the years, the following people have helped make BikeWalkRoll possible: Devon Mitton (web wizard #2), Torrin Swanson (campaign and mobile developer), Christine Houde, Sue Barkman, Randall McQuaker, Jackie Avent, Shoni Madden, Lea Grzenda, Janice Lukes, Tracy Hucul, Masha Giller, Jamie Hilland, Red River College, The University of Manitoba, The City of Winnipeg, Adam Prokopanko, Steve Lawrence, Toronto Catholic School Division, The National Safe Routes To School Partnership, Taylor Lonsdale, The Winnipeg Foundation, The Heart and Stroke Foundation, Jeannette Montufar, Jamie Lacoste, Leigh Anne Parry, Velo-City Vienna, Walk21, Winter Cycling Federation, Torrin Swanson, International Winter Bike to Work Day, Neil Bailey, Green Communities Canada, school division, students and teachers everywhere!</p>
+      </div>
     </ContentMain>
   </div>
 </template>
@@ -37,7 +40,7 @@ import SubHeader from '@/components/header/SubHeader.vue';
 import ContentMain from '@/components/content/Main.vue';
 
 export default {
-  name: 'Faq',
+  name: 'About',
   components: {
     ContentMain,
     SubHeader,
--- src/views/Faq.vue
+++ src/views/Faq.vue
@@ -1,11 +1,36 @@
 <template>
-  <div class="about">
-    <h1>This is an FAQ page</h1>
+  <div class="faq static-page">
+    <SubHeader title="FAQ/Support"></SubHeader>
+    <ContentMain title="" :dark=true>
+      <div class="max-w-screen-lg m-auto p-6 bg-white">
+        <h1><a href="https://docs.google.com/document/d/1OKxRiyW5r7A46xVaZ2SV2UZZl9jGlNYDQ_59E-Bfpvk/edit#heading=h.xn13qo8b11k0" target="_blank">View the QUICK START GUIDE for Teachers</a></h1>
+        <p>This guide will help you get going fast and tell you the basics of how BikeWalkRoll works.</p>
+        <h3><a href="https://docs.google.com/document/d/1esq0z5w0SqiV_ccq790NRG3hTVxohFG9iA7VVyaw60g/edit#heading=h.xn13qo8b11k0" target="_blank">View the FAQ and Troubleshooting guide</a></h3>
+        <p>See frequently asked questions, get information on advanced stuff and find resolutions for common issues.</p>
+        <h3><a href="https://docs.google.com/document/d/1jv6Gzc-glgp9pgv7ceE8ewFwITkGLm8JT3mBj0wlAJI" target="_blank">View the Champion tools guide</a></h3>
+        <p>Learn about the tools available to those with champion accounts.</p>
+        <h3><a href="/pdfs/Overview_of_BikeWalkRoll_Data_Collection_and_Management_March_2019.pdf" target="_blank">Download an Overview of BikeWalkRoll Data Management, Privacy and Permissions (PDF)</a></h3>
+        <p>This document provides information regarding BikeWalkRoll data security and privacy. It may be helpful if you are required to seek approval from a school board/division/district to use BikeWalkRoll for school travel data collection.</p>
+        <h4>Need more help or advice getting started?</h4>
+        <p>To learn more about BikeWalkRoll and get friendly answers to common questions about school-based transportation and what we can do about it, talk to a local active school travel organization near you. There is a good chance they are familiar with BikeWalkRoll and can help you get started or answer common technical questions. It may be your local school administration, or an NGO working on children’s mobility. If you are part of a data collection campaign, contact the person who reached out to you. Have a look at this <a href="https://bikewalkroll.org/Organizations/">list of helpful organizations</a> for one near you! </p>
+        <h4>Running into technical issues?</h4>
+        <p>For basic technical support, please email <a href="mailto:Support@bikewalkroll.org">Support@bikewalkroll.org</a>.</p>
+        <p>For urgent technical support questions, please send an email to <a href="mailto:UrgentSupport@BikeWalkRoll.org">UrgentSupport@BikeWalkRoll.org</a> describing your issue in detail. These inquiries will be prioritized.</p>
+      </div>
+    </ContentMain>
+    
   </div>
 </template>
 
 <script>
+import SubHeader from '@/components/header/SubHeader.vue';
+import ContentMain from '@/components/content/Main.vue';
+
 export default {
   name: 'Faq',
+  components: {
+    ContentMain,
+    SubHeader,
+  },
 };
 </script>

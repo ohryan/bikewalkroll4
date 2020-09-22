1f7e7a1dfb42c78ff0b7a0d5fad254be0758295b
Ryan Neudorf <ryan.neudorf@gmail.com>
2020-09-21 22:53:08 -0500

    Added postcss-nested.

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
 	"plugins": [
- 		require('tailwindcss')('tailwind.config.js'),
- 		require('autoprefixer')(),
+		 require('tailwindcss')('tailwind.config.js'),
+		 require('postcss-nested'),
+		 require('autoprefixer')(),
 	]
 }
